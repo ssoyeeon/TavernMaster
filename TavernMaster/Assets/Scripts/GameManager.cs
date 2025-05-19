a92b02bd1b2cda5cac37d316e3a4fe26a949ffac
ssoyeeon <ssuu2039@gmail.com>
2025-05-19 15:47:27 +0900

    [fix] 도트 수정 및 맵 전환
    Asprite 쓸 때 160X160 및 화질 400 맞추기. 꼭!

--- TavernMaster/Assets/Scripts/GameManager.cs
+++ TavernMaster/Assets/Scripts/GameManager.cs
@@ -1,25 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public float gameTimer;     //게임 플레이 타임 저장용 
     public float timer;         //시간 ( 게임 시간 ) 
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
         gameTimer += Time.deltaTime;
+        if (SceneManager.GetActiveScene().buildIndex == 2)       //인덱스 2라면 
+        {
+            GameTime();
+        }
     }
 
-    public void GameTime()
+    public void GameTime()  //게임 시간 정해주기 현실 20초 = 게임 10분
     {
-        
+        timer += Time.deltaTime;        //게임 시간 더해주기
+        switch(timer)       //for문으로 바꾸면 더 쉽지 않을까~.. 하지만 스위치 쓰고 싶어요.
+        {
+              case 20:
+                   Debug.Log("6시 10분");
+                   break;
+              case 40:
+                   Debug.Log("6시 20분");
+                   break;
+                   //계속 추가해놓기 
+        }
+    }
+
+    public enum PlayStates
+    {
+        Sleep,      //수면
+        House,      //집
+        Farm,       //농장
+        Play        //일반 플레이 중
     }
 }
--- TavernMaster/Assets/Scripts/PlayerMove.cs
+++ TavernMaster/Assets/Scripts/PlayerMove.cs
@@ -15,11 +15,38 @@ public class PlayerMove : MonoBehaviour
 
     void FixedUpdate()
     {
+        Movement();
+    }
+
+    public void Movement()
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
         Vector2 movement = new Vector2(horizontal, vertical);
 
         rb.velocity = movement * moveSpeed;
+
+        //고정으로 바꾸기
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
     }
+
+    //땅인거 먼저 체크하고 할거야 아니면 요기 함수 안에서 체크할고야?
+    public void Homi()
+    {
+        
+    }
+
+    public void Water()
+    {
+
+    }
+
 }
